b995651c54c8c6a8eb288d516c2d3990b09f2cfd
DiosaEstelaa <estelagm0109@gmail.com>
2022-06-22 12:52:20 +0200

    ultima letra

--- repasoVerano/src/repasoverano/Ejercicio_1.java
+++ repasoVerano/src/repasoverano/Ejercicio_1.java
@@ -19,7 +19,7 @@ public class Ejercicio_1 {
         // TODO code application logic here
 
         String nombre = "Estela";
-        System.out.println("Mi nombre es" + nombre);
+        System.out.println("Mi nombre es " + nombre);
         System.out.println("Mi nombre tiene " + nombre.length() + " letras.");
         System.out.println("La primera letra de mi nombre es " + nombre.charAt(0));
         System.out.println("La segunda letra de mi nombre es " + nombre.charAt(1));
@@ -27,6 +27,11 @@ public class Ejercicio_1 {
         System.out.println("La cuarta letra de mi nombre es " + nombre.charAt(3));
         System.out.println("La quinta letra de mi nombre es " + nombre.charAt(4));
         System.out.println("La sexta letra de mi nombre es " + nombre.charAt(5));
+        
+        int ultimaLetra;
+        ultimaLetra=nombre.length();
+        System.out.println("Y la última letra de " + nombre + " es " + nombre.charAt(ultimaLetra-1));
+        
 
     }//fin main
 }//fin class
