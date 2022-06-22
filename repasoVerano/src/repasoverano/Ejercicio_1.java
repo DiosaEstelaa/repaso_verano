cb6374a344335b832c01c8c5a6de9cc22356d85c
DiosaEstelaa <estelagm0109@gmail.com>
2022-06-22 12:48:48 +0200

    primer ejercicio
    repaso muy básico de Strings

---
+++ repasoVerano/src/repasoverano/Ejercicio_1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasoverano;
+
+/**
+ *
+ * @author estel
+ */
+// v11
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String nombre = "Estela";
+        System.out.println("Mi nombre es" + nombre);
+        System.out.println("Mi nombre tiene " + nombre.length() + " letras.");
+        System.out.println("La primera letra de mi nombre es " + nombre.charAt(0));
+        System.out.println("La segunda letra de mi nombre es " + nombre.charAt(1));
+        System.out.println("La terfcera letra de mi nombre es " + nombre.charAt(2));
+        System.out.println("La cuarta letra de mi nombre es " + nombre.charAt(3));
+        System.out.println("La quinta letra de mi nombre es " + nombre.charAt(4));
+        System.out.println("La sexta letra de mi nombre es " + nombre.charAt(5));
+
+    }//fin main
+}//fin class
